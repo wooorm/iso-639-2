eadf0796b021e417db024a039748c5f5c5a057f4
wooorm <tituswormer@gmail.com>
2014-11-23 16:27:06 +0100

    Refactor `script/build-support.js`

--- script/build-support.js
+++ script/build-support.js
@@ -1,32 +1,42 @@
 'use strict';
 
-var fs,
-    languages;
+/**
+ * Dependencies.
+ */
 
+var languages,
+    fs,
+    table;
+
+languages = require('..');
 fs = require('fs');
-languages = require('../').all();
+table = require('markdown-table');
+
+/**
+ * Write.
+ */
 
 fs.writeFileSync('Support.md',
     'Supported Codes\n' +
     '=================\n' +
     '\n' +
-    '| ISO-639-2 | ISO-639-2T | ISO-639-1 | Name |\n' +
-    '| :-------: | :--------: | :-------: | :--: |\n' +
+    table(
+        [
+            ['ISO-639-2', 'ISO-639-2T', 'ISO-639-1', 'Name']
+        ].concat(languages.keys().map(function (code) {
+            var language;
 
-    Object.keys(languages).map(function (code) {
-        var language;
+            language = languages.get(code);
 
-        language = languages[code];
-
-        return '| ' +
-            [
+            return [
                 code,
                 language.terminologic || '',
                 language.iso6391 || '',
                 language.name
-            ].join(' | ') +
-            ' |';
-    }).join('\n') +
-
+            ];
+        })), {
+            'align': ['c', 'c', 'c', 'l']
+        }
+    ) +
     '\n'
 );

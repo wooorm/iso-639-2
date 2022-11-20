8e2a646ac924826c1955461442a4b4608e185af6
Titus Wormer <tituswormer@gmail.com>
2022-11-20 09:49:39 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,21 +1,13 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {iso6392} from './index.js'
 
-test('iso6392', function (t) {
-  let index = -1
-
-  t.plan(5)
-
-  t.ok(Array.isArray(iso6392), 'should be an `array`')
-
-  while (++index < iso6392.length) {
-    if (iso6392[index].iso6391 !== 'en') {
-      continue
-    }
-
-    t.equal(iso6392[index].iso6392B, 'eng', 'bibliographic code')
-    t.equal(iso6392[index].iso6392T, undefined, 'terminologic code')
-    t.equal(iso6392[index].iso6391, 'en', '639-1 code')
-    t.equal(iso6392[index].name, 'English', 'name')
-  }
+test('iso6392', function () {
+  assert.ok(Array.isArray(iso6392), 'should be an `array`')
+  const en = iso6392.find((d) => d.iso6391 === 'en')
+  assert(en)
+  assert.equal(en.iso6392B, 'eng', 'bibliographic code')
+  assert.equal(en.iso6392T, undefined, 'terminologic code')
+  assert.equal(en.iso6391, 'en', '639-1 code')
+  assert.equal(en.name, 'English', 'name')
 })

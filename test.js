eed6fd436d09602c92c438187c8db9f33b6db1c3
Titus Wormer <tituswormer@gmail.com>
2023-06-03 17:11:05 +0200

    Add tests for exposed identifiers

--- test.js
+++ test.js
@@ -2,7 +2,13 @@ import assert from 'node:assert/strict'
 import test from 'node:test'
 import {iso6392} from './index.js'
 
-test('iso6392', function () {
+test('iso6392', async function () {
+  assert.deepEqual(
+    Object.keys(await import('./index.js')).sort(),
+    ['iso6392', 'iso6392BTo1', 'iso6392BTo2T', 'iso6392TTo1', 'iso6392TTo2B'],
+    'should expose the public api'
+  )
+
   assert.ok(Array.isArray(iso6392), 'should be an `array`')
   const en = iso6392.find((d) => d.iso6391 === 'en')
   assert(en)
